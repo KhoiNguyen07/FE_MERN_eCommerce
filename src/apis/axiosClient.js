daf33433ac850d532a22fe2cb016a21551c0b776
sodangdev <dangkhoinguyen1501@gmail.com>
2025-08-31 04:31:07 +0700

    fe

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const api = import.meta.env.VITE_DB_URL;
 
 const axiosClient = axios.create({
-  baseURL: api,
+  baseURL: "https://techleaf.pro/shoes-shop",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json"
