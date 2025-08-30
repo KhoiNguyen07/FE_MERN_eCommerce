fb842431563d2ddb76bb430471e9f6018c8fb09b
sodangdev <dangkhoinguyen1501@gmail.com>
2025-08-30 19:55:44 +0700

    fe

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://be-mern-ecommerce-4os7.onrender.com",
-  // baseURL: "http://localhost:8080",
+  baseURL: "https://techleaf.pro/shoes-shop",
+  // baseURL: "http://localhost:8080/shoes-shop",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json"
