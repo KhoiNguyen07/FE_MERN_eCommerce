7c7e2aa265747868844e2ef16542878460d4072e
sodangdev <dangkhoinguyen1501@gmail.com>
2025-09-05 10:15:47 +0700

    FE

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -3,7 +3,7 @@ const apilocal = import.meta.env.VITE_DB_URL_LOCAL;
 const apiRender = import.meta.env.VITE_DB_URL_RENDER;
 
 const axiosClient = axios.create({
-  baseURL: "https://be-mern-ecommerce-4os7.onrender.com/shoes-shop",
+  baseURL: apiRender,
   timeout: 30000,
   headers: {
     "Content-Type": "application/json"
